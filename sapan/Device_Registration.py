2089fc619db99db3dc1f84cb3ecc78c2e617d872
sapan2211 <sapan2211@yahoo.com>
2017-04-06 01:58:18 -0500

    Add files via upload
    Device Registration. Implemented post

--- sapan/Device_Registration.py
+++ sapan/Device_Registration.py
@@ -1,25 +1,31 @@
 import requests
 import time
-import RPi.GPIO as gpio
+#import RPi.GPIO as gpio
 
-gpio.setmode(gpio.BOARD)
-gpio.setup(11, gpio.OUT)
+#gpio.setmode(gpio.BOARD)
+#gpio.setup(11, gpio.OUT)
 
 url = "http://35.162.32.72"
 geturl = "http://35.162.32.72:8080/light/status"
 DeviceId="CLOUD001"
 payload= {'DeviceId': DeviceId}
 import json
-#r = requests.post(url, data=json.dumps(payload))
+
+body = {'DeviceId':DeviceId}
+header = {'Content-Type':'application/json', 'Accept':'application/json'}
+response = requests.post('"http://35.162.32.72:8080/registerdevice/pi', headers = header, data = json.dumps(body), verify = False)
+if response.status_code == 200:
+    print "Registered"
+
+	
 rget = requests.get(geturl,params = payload,verify = False)
 while rget.status_code!=404:
         rget = requests.get(geturl,params = payload,verify = False)
         data= json.loads(rget.text)
         if data["light"]=="off":
                 print 0
-                gpio.output(11, gpio.LOW)
+                #gpio.output(11, gpio.LOW)
         elif data["light"]=="on":
                 print 1
-                gpio.output(11, gpio.HIGH)
-        time.sleep(0.5)
-
+                #gpio.output(11, gpio.HIGH)
+        time.sleep(1)
