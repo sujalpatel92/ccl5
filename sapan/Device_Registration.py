aad467bf53e9e1d39bc8f71fd89aed2e2a6f4e61
MaitriJ <maitrimaitri07@gmail.com>
2017-04-06 18:40:36 -0500

    code for logging in PI

--- sapan/Device_Registration.py
+++ sapan/Device_Registration.py
@@ -1,9 +1,17 @@
 import requests
 import time
+
+#code for log
+import logging
+
 #import RPi.GPIO as gpio
 
 #gpio.setmode(gpio.BOARD)
 #gpio.setup(11, gpio.OUT)
+
+#code for log
+LOG_FILENAME = "pi_logs.txt"
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 url = "http://35.162.32.72"
 geturl = "http://35.162.32.72:8080/light/status"
@@ -16,6 +24,7 @@ header = {'Content-Type':'application/json', 'Accept':'application/json'}
 response = requests.post('"http://35.162.32.72:8080/registerdevice/pi', headers = header, data = json.dumps(body), verify = False)
 if response.status_code == 200:
     print "Registered"
+    logging.debug("PI_Registered")
 
 	
 rget = requests.get(geturl,params = payload,verify = False)
@@ -24,8 +33,10 @@ while rget.status_code!=404:
         data= json.loads(rget.text)
         if data["light"]=="off":
                 print 0
+                logging.debug("Light_Status: OFF")
                 #gpio.output(11, gpio.LOW)
         elif data["light"]=="on":
                 print 1
+                logging.debug("Light_Status: ON")
                 #gpio.output(11, gpio.HIGH)
         time.sleep(1)
