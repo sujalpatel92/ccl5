1785f3eacf82dccd0c6dd16179dc83e82fdc684e
sapan2211 <sapan2211@yahoo.com>
2017-04-09 23:11:57 -0500

    Final Upload Device_Registration

--- sapan/Device_Registration.py
+++ sapan/Device_Registration.py
@@ -4,14 +4,14 @@ import logging
 LOG_FILENAME = "pi_logs.txt"
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG,format='%(asctime)s %(message)s')
 
-Email = "[Registered Email Id]"
-DeviceId="[your DeviceId from Get_DeviceId.py]"
+Email = "sapan2211@yahoo.com"
+DeviceId="88521B7AD79BE277"
 payload= {'Email':Email,'DeviceId': DeviceId}
 import json
 
 body = {'Email':Email,'DeviceId':DeviceId}
 header = {'Content-Type':'application/json', 'Accept':'application/json'}
-response = requests.post('http://35.162.32.72:8000/registerdevice/pi', headers = header, data = json.dumps(body), verify = False)
+response = requests.post('http://35.162.32.72:8005/registerpi/registerdevice', headers = header, data = json.dumps(body), verify = False)
 if response.status_code == 200:
 	print "Registered"
 	logging.debug("PI_Registered with ID"+ DeviceId)
