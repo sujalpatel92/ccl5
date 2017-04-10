2ffc46c294023dbba34723b66f4abb04cfca87d8
Sujal Patel <patelsujal3392@gmail.com>
2017-04-10 04:06:52 +0000

    Hopefully final server code

--- iot_platform/iot_platform.py
+++ iot_platform/iot_platform.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import logger
+import sqlite3
 import bottle, json, time, sys, os, datetime, random
 from bottle import get,post,request,response,route,run,Bottle
 from rocket import Rocket
@@ -10,9 +11,9 @@ print_debug = True
 # store generated id's based on request from pi
 id_given_to_pi = list()
 # store id's waiting to be registered from web
-confirm_id_from_web = list()
+confirm_id_from_web = dict()
 # store the registered_pi's
-registered_pi = list()
+registered_pi = dict()
 # maintain time of last access by pi
 pi_last_connected = dict()
 # maintain the state of LED of connected pi's. By default it is "off"
@@ -61,6 +62,29 @@ app.debug = True
 
 # start bottle app REST API definitions
 """
+Endpoint to connect to platform from pi
+"""
+@app.post('/connect/pi')
+def connect_pi():
+    global platform_log, device_log, registered_pi, pi_last_connected
+    payload = json.loads(json.dumps(request.json), object_hook = JsonDecode.get_dict)
+    pi_id = payload["DeviceId"]
+    email = payload["Email"]
+    if pi_id in registered_pi:
+        if registered_pi[pi_id] != email:
+            platform_log.error("Incorrect connection attempt from device ID: %s", pi_id)
+            return bottle.HTTPResponse(status = 404)
+        elif registered_pi[pi_id] == email:
+            now = datetime.datetime.now()
+            pi_last_connected[pi_id] = now
+            platform_log.info("Connected to Device ID: %s", pi_id)
+            device_log[pi_id].info("Connected to IoT Platform")
+            return bottle.HTTPResponse(status = 200)
+    else:
+        platform_log.error("Incorrect connection attempt from unregistered device using ID: %s", pi_id)
+        return bottle.HTTPResponse(status = 404)
+
+"""
 Endpoint to get device id for pi
 """
 @app.get('/registerpi/get_pi_id')
@@ -87,6 +111,7 @@ def register_pi_from_device():
     if print_debug:
         print("ID received: ",payload["DeviceId"])
     pi_id = payload["DeviceId"]
+    email = payload["Email"]
     if print_debug:
         print("Request to register device ID: ",pi_id)
     if pi_id in registered_pi:
@@ -101,7 +126,7 @@ def register_pi_from_device():
         #setup pi last connected here
         if pi_id in id_given_to_pi:
             id_given_to_pi.remove(pi_id)
-            confirm_id_from_web.append(pi_id)
+            confirm_id_from_web[pi_id] = email
         platform_log.info("Register request received from pi for ID: %s", pi_id)
         if print_debug:
             print("Register request received from pi for ID: ",pi_id)
@@ -117,9 +142,18 @@ def get_pi_confirmation():
         print(request.params["pi_id"])
         print(confirm_id_from_web)
     pi_id = str(request.params["pi_id"]).split('|')[0]
+    email = str(request.params["pi_id"]).split('|')[1]
+    location = str(request.params["pi_id"]).split('|')[2]
     if pi_id in confirm_id_from_web:
-        confirm_id_from_web.remove(pi_id)
-        registered_pi.append(pi_id)
+        if confirm_id_from_web[pi_id] != email:
+            platform_log.error('Wrong Email ID requested to register from front end. Incorrect Email ID: %s', email)
+            if print_debug:
+                print('Wrong Email ID requested to register from front end')
+            return bottle.HTTPResponse(status = 404)
+
+        del confirm_id_from_web[pi_id]
+        registered_pi[pi_id] = email
+        insert_device_details_in_db(pi_id, email, location)
         pi_LED_state[pi_id] = "off"
         platform_log.info("Device Successfully registered with ID: %s", pi_id)
         device_log[pi_id] = logger.get_custom_logger(pi_id)
@@ -147,7 +181,8 @@ def deregister_pi():
         print(request.params["pi_id"])
     pi_id = str(request.params["pi_id"]).split('|')[0]
     if pi_id in registered_pi:
-        registered_pi.remove(pi_id)
+        del registered_pi[pi_id]
+        delete_device_details_in_db(pi_id)
     else:
         bottle.HTTPResponse(status = 500)
     if pi_id in pi_LED_state:
@@ -158,9 +193,11 @@ def deregister_pi():
     if pi_id in device_log:
         device_log[pi_id].info("PI deregistered.")
         del device_log[pi_id]
+    if pi_id in pi_last_connected:
+        del pi_last_connected[pi_id]
     if print_debug:
+        return bottle.HTTPResponse(status = 200)
         print("PI deregistered")
-    return bottle.HTTPResponse(status = 200)
 
 """
 Endpoint to get light state from pi
@@ -190,6 +227,14 @@ def set_LED_status():
         print(payload['pi_id'])
     led_state = payload['light']
     pi_id = payload['pi_id']
+    if pi_id in pi_last_connected:
+        now = datetime.datetime.now()
+        last_time = pi_last_connected[pi_id]
+        difference = now - last_time
+        if int(difference) > 5 :
+            platform_log.error("PI has not been connected to platform for more than 5 seconds. ID: %s", pi_id)
+            device_log[pi_id].error("PI has not been connected to platform for more than 5 seconds.")
+            return bottle.HTTPResponse(status = 404)
     pi_LED_state[pi_id] = led_state
     platform_log.info("Received Light State %s for PI ID: %s from WEB UI", led_state, pi_id)
     device_log[pi_id].info('Received Light State %s from front end', led_state)
@@ -222,6 +267,46 @@ def get_logs_iot_ip():
 # end bottle app REST API definitions
 # start regular functions
 """
+This function populates global variables
+"""
+def get_registered_devices_from_db():
+    global registered_pi
+    conn = sqlite3.connect('devices.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM devices")
+    data = cur.fetchall()
+    if len(data) == 0:
+        conn.close()
+        return
+    elif len(data) > 0:
+        for item in data:
+            pi_id = str(item[0])
+            email = str(item[1])
+            registered_pi[pi_id] = email
+        conn.close()
+        return
+"""
+This functions inserts pi details into database
+"""
+def insert_device_details_in_db(pi_id, email, location):
+    conn = sqlite3.connect('devices.db')
+    cur = conn.cursor()
+    cur.execute("insert into devices values (?, ?, ?)", (pi_id, email, location))
+    conn.commit()
+    conn.close()
+    return
+
+"""
+This function deletes a row from database table
+"""
+def delete_device_details_in_db(pi_id):
+    conn = sqlite3.connect('devices.db')
+    cur = conn.cursor()
+    cur.execute("delete from devices where id = ?",(pi_id,))
+    conn.commit()
+    conn.close()
+    return
+"""
 This function generates ID for pi
 """
 def generateID():
@@ -249,4 +334,5 @@ def run_server():
 
 # code entry point
 if __name__ == "__main__" :
+    get_registered_devices_from_db()
     run_server()
