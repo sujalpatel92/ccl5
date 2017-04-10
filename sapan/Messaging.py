83c0b77f0d171f64076e13c231dd6b2e58d89570
sapan2211 <sapan2211@yahoo.com>
2017-04-09 23:12:53 -0500

    Final Upload.. Added Email in Payload

--- sapan/Messaging.py
+++ sapan/Messaging.py
@@ -10,26 +10,38 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG,format='%(asctime
 gpio.setmode(gpio.BOARD)
 gpio.setup(11, gpio.OUT)
 
+
+
 url = "http://35.162.32.72"
-geturl = "http://35.162.32.72:8000/light/status"
-DeviceId="[your DeviceID]"
-payload= {'DeviceId': DeviceId}
+geturl = "http://35.162.32.72:8005/light/status"
+connecturl = "http://35.162.32.72:8005/connect/pi"
 
+DeviceId="88521B7AD79BE277"
+Email = "sapan2211@gmail.com"
+payload= {'DeviceId': DeviceId, 'Email': Email}
 
-
-rget = requests.get(geturl,params = payload,verify = False)
+flag=0
+logging.debug("Connected to IoT Platform")
+rget = requests.get(connecturl,params = payload,verify = False)
 while rget.status_code!=404:
-        rget = requests.get(geturl,params = payload,verify = False)
+	logging.debug("Fetching Light State from IoT Platform")
+	rget = requests.get(geturl,params = payload,verify = False)
         if rget.status_code == 404:
-                break
-        data= json.loads(rget.text)
+		break
+	data= json.loads(rget.text)
         if data["light"]=="off":
                 print 0
                 gpio.output(11, gpio.LOW)
-                logging.debug("Light_Status: OFF")
+		if flag==1:
+			logging.debug("Light_Status: OFF")
+			flag=0
         elif data["light"]=="on":
                 print 1
                 gpio.output(11, gpio.HIGH)
-                logging.debug("Light_Status: ON")
+		if flag==0:
+			logging.debug("Light_Status: ON")
+			flag=1
         time.sleep(1)
-
+	logging.debug("Connected to IoT Platform")
+	rget = requests.get(connecturl, params = payload, verify = False)
+logging.debug("Device Disconnected")
